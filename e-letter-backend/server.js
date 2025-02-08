84a02eb99f83b118a2fe342609933c8d4cb57336
unatteksen <unatteksen@gmail.com>
2025-02-08 23:17:45 +0300

    update db

--- e-letter-backend/server.js
+++ e-letter-backend/server.js
@@ -15,12 +15,9 @@ app.use('/api/users', userRoutes);
 app.use('/api/letters', letterRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/eletter', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Failed to connect to MongoDB', err));
+mongoose.connect('mongodb://localhost:27017/eletter')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 // Start the server
 const PORT = process.env.PORT || 5000;
